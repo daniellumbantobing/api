847491c9cb2acf7a403fb134b7d183e547222340
daniel tobing <daniellumbantobing05@gmail.com>
2020-11-04 14:16:09 +0700

    Create new Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
+use App\Http\Requests\ProductRequest;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
+use PhpParser\Node\Expr\Print_;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +41,19 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response([
+            'date' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
